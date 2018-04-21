7c67825bfb93e815a29c8e56fb820102e5f4dcaa
Ahmed M. Osman <amosman177@gmail.com>
2018-04-21 10:30:05 -0700

    Fixed path to my image and created an app directory to contain my HTML file.

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000
 
-app.use(express.static(__dirname + /assessment/));
+app.use(express.static(__dirname + /app/));
 
 app.listen(port, function(){
 	console.log('Server starting at http://localhost:' + port);
-})
+});
